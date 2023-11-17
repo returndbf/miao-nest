68ccd09d2cb7f77e43568c7aa54a7561e74d6a5c
大笨峰 <1668959325@qq.com>
2023-11-17 18:28:34 +0800

    fix: 添加store entity

--- src/store/entities/store.entity.ts
+++ src/store/entities/store.entity.ts
@@ -1,1 +1,17 @@
-export class Store {}
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Store {
+  @Column()
+  id: string;
+  @Column()
+  goods: string;
+  @Column()
+  rewards: number;
+  @Column()
+  icon: string;
+  @Column()
+  description: string;
+  @Column()
+  del_flag: number;
+}
--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
+import { Store } from './entities/store.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Store])],
   controllers: [StoreController],
   providers: [StoreService],
 })
